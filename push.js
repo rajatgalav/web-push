3e8922b2216bca333d102b26f2c389f92f4e0890
rajatgalav <rajatgalav1295@gmail.com>
2022-06-01 17:54:41 +0530

    test push

--- push.js
+++ push.js
@@ -7,6 +7,6 @@ let vapidkeys = {
 
 push.setVapidDetails('mailto:test@gmail.com', vapidkeys.publicKey, vapidkeys.privateKey);
 
-let sub = {};
+let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/ceuM5Gyg4Y0:APA91bE-C_XDzCF7k_FftEIUsnZTCUMVY-kz1Me2Hom6lFmEeX2pp3e9sofRnpxReQs4ARp1pNoPKHMAEa1oOhGnC81prv1WpaxZ5kGnFeKMgWRRPAygaIt_cLwyrlVbo_wKFxRUDf1l","expirationTime":null,"keys":{"p256dh":"BM1bsvQCG4aGTFFkYRSnkXhAp04J9y6Vdt2dNmQMas-UE1-l8Svuoz7y48QlKbzNc7Kt1WnpnoL17R3XJ-J46qQ","auth":"BFSoURA9EUHmC8beq2jvUA"}}
 
 push.sendNotification(sub, 'test message');
